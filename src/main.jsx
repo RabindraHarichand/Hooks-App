e45651edade64ce0b408e5a102afcbf905eb1460
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-14 14:40:00 -0400

    [Section 9]: Definig useCounter Custom Hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import { HooksApp } from "./HooksApp";
+// import { CounterApp } from "./01-useState/CounterApp";
 
 import "./index.css";
-import { CounterApp } from "./01-useState/CounterApp";
+import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CounterApp />
+    <CounterWithCustomHook />
   </StrictMode>
 );
