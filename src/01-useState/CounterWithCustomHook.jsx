5114df20919d4d1730061aa55d206c41ceddac43
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-14 15:00:51 -0400

    [Section 9]: Expose custom hooks

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import { useCounter } from "../hooks/useCounter";
 
 export const CounterWithCustomHook = () => {
-  const { counter } = useCounter();
+  const { counter, decrement, increment, reset } = useCounter();
 
   return (
     <>
       <h1>CounterWithCustomHook: {counter}</h1>
       <hr />
-      <button className="btn btn-primary">+1</button>
-      <button className="btn btn-primary">Reset</button>
-      <button className="btn btn-primary">-1</button>
+      <button className="btn btn-primary" onClick={() => increment(2)}>
+        +1
+      </button>
+      <button className="btn btn-primary" onClick={reset}>
+        Reset
+      </button>
+      <button className="btn btn-primary" onClick={() => decrement()}>
+        -1
+      </button>
     </>
   );
 };
