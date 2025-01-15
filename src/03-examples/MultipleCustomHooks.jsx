319fc71b0cf793eac660a7b558918d2f69722b55
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-15 12:50:37 -0400

    [Section 9]: Add useFetch Custom Hook

---
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useFetch } from "../hooks";
+
+export const MultipleCustomHooks = () => {
+  useFetch();
+
+  return <div>MultipleCustomHooks</div>;
+};
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+export const useFetch = () => {
+  const [state, setstate] = useState({
+    data: null,
+    isLoading: true,
+    hasError: false,
+    error: null,
+  });
+
+  useEffect(() => {
+    getFetch();
+  }, []);
+
+  const getFetch = async () => {
+    const resp = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+    const data = await resp.json();
+
+    console.log({ data });
+  };
+
+  return {
+    data: state.data,
+    isLoading: state.isLoading,
+    hasError: state.hasError,
+    error: state.error,
+  };
+};
