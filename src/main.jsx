1d89b1f5dd44980dc049ade5e80a2ef462ce776c
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-15 16:55:32 -0400

    [Section 9]: useMemo usage

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,13 @@ import { createRoot } from "react-dom/client";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { Layout } from "./05-useLayoutEffect/Layout";
-import { Memorize } from "./06-memos/Memorize";
+// import { Memorize } from "./06-memos/Memorize";
+import { MemoHook } from "./06-memos/MemoHook";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Memorize />
+    <MemoHook />
   </StrictMode>
 );
