61ff4350d01c8e5f5ca9e066a4f9dd8136999896
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-15 09:13:58 -0400

    [Section 9]: UseEffect in SimpleForm component

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    username: "strider",
+    email: "test@google.com",
+  });
+
+  const { username, email } = formState;
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    console.log("useEffect called");
+  });
+
+  return (
+    <>
+      <h1>Formulario Simple</h1>
+      <hr />
+
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Username"
+        name="username"
+        value={username}
+        onChange={onInputChange}
+      />
+
+      <input
+        type="email"
+        className="form-control mt-2"
+        placeholder="test@google.com"
+        name="email"
+        value={email}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
