1d9e9889f16aa9a178f01b1df6332f977d52f97b
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-15 16:35:20 -0400

    [Section 9]: Add memo. This allows react components to re-render only when one of its props has changed. If there is no change then it won't re-render

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,13 @@ import { createRoot } from "react-dom/client";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHooks";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
-import { Layout } from "./05-useLayoutEffect/Layout";
+// import { Layout } from "./05-useLayoutEffect/Layout";
+import { Memorize } from "./06-memos/Memorize";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Layout />
+    <Memorize />
   </StrictMode>
 );
