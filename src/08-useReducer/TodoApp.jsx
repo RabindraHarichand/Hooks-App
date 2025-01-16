bd57b30a918f84e0eb9b57c839bf81964fd13ae4
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-16 11:07:55 -0400

    [Section 10]: Delete TODO Item

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -36,6 +36,12 @@ export const TodoApp = () => {
     dispatch(action);
   };
 
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: "[TODO] Remove Todo",
+      payload: id,
+    });
+  };
   return (
     <>
       <h1>
@@ -46,7 +52,7 @@ export const TodoApp = () => {
       <div className="row">
         <div className="col-7">
           {/* TodoList */}
-          <TodoList todos={todos} />
+          <TodoList todos={todos} onDeleteTodo={(id) => handleDeleteTodo(id)} />
           {/* Fin TodoList */}
         </div>
 
