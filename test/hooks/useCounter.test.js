f6dd1eb700b958c5027c1a02645c7c0f22c3da8d
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-17 11:36:21 -0400

    [Section 12 - Testing]: useCounter custom hook. Testing decrement, increment & reset

--- test/hooks/useCounter.test.js
+++ test/hooks/useCounter.test.js
@@ -1,5 +1,6 @@
 import { renderHook } from "@testing-library/react";
 import { useCounter } from "../../src/hooks/useCounter";
+import { act } from "react";
 
 describe("Pruebas en el useCounter", () => {
   test("debe retornar los valores por defecto", () => {
@@ -19,5 +20,44 @@ describe("Pruebas en el useCounter", () => {
     const { counter } = result.current;
 
     expect(counter).toBe(100);
+  });
+
+  test("debe incrementar el contador", () => {
+    const { result } = renderHook(() => useCounter(100));
+
+    const { counter, increment } = result.current;
+
+    act(() => {
+      increment();
+      increment(2);
+    });
+
+    expect(result.current.counter).toBe(103);
+  });
+
+  test("debe decrementar el contador", () => {
+    const { result } = renderHook(() => useCounter(100));
+
+    const { counter, decrement } = result.current;
+
+    act(() => {
+      decrement();
+      decrement(2);
+    });
+
+    expect(result.current.counter).toBe(97);
+  });
+
+  test("debe resetear el contador", () => {
+    const { result } = renderHook(() => useCounter(100));
+
+    const { counter, decrement, reset } = result.current;
+
+    act(() => {
+      decrement();
+      reset();
+    });
+
+    expect(result.current.counter).toBe(100);
   });
 });
