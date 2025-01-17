7c5ce8fa9d637b3f8bda77df5324c8c9a13ab165
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-17 18:13:32 -0400

    [Section 12 - Testing]: MainApp. Testing AppRouter

---
+++ test/09-useContext/MainApp.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { MainApp } from "../../src/09-useContext/MainApp";
+import { MemoryRouter } from "react-router";
+
+describe("Pruebas en MainApp", () => {
+  test("debe mostrar el HomePage", () => {
+    render(
+      <MemoryRouter>
+        <MainApp />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("HomePage")).toBeTruthy();
+    // screen.debug();
+  });
+
+  test("debe mostrar el LoginPage", () => {
+    render(
+      <MemoryRouter initialEntries={["/login"]}>
+        <MainApp />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("LoginPage")).toBeTruthy();
+    // screen.debug();
+  });
+});
